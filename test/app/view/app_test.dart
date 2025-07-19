79ee74325422eb36c034778189baf647fe514720
Chifunilo <130912326+Chifunilo@users.noreply.github.com>
2025-07-19 16:17:49 +0200

    added cubit and mason

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:smartpocket_app/app/app.dart';
-import 'package:smartpocket_app/counter/counter.dart';
+import 'package:smartpocket_app/features/auth/login/Login_page.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(LoginPage()), findsOneWidget);
     });
   });
 }
